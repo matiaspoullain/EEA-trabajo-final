b2ca001fa005ffae9341eccaa7a37f6153b05da9
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2021-12-05 12:30:36 -0300

    cosas para la teorica

--- Scripts/Imagenes teorica.R
+++ Scripts/Imagenes teorica.R
@@ -142,3 +142,102 @@ modelo.concava.aumentado <- augment(modelo.concava)
 (plot.concava <- res.vs.pred(modelo.concava.aumentado))
 
 ggsave("Imagenes/Teorica/Plot concava.png", plot.concava, width = 4, height = 4)
+
+
+
+## Correcciones que no son modelados de varianza:
+#Otra variable en juego:
+
+set.seed(3)
+
+n.datos <- 50
+
+var1 <- runif(n.datos, min = 0, max = 100)
+
+var2 <- runif(n.datos, min = 0, max = 100)
+
+var3 <- runif(n.datos, min = 0, max = 100)
+
+target1 <- 200 + 0 * var1 + rnorm(n.datos, sd = 20)
+
+target2 <- 200 + 1 * var2 + rnorm(n.datos, sd = 20)
+
+target3 <- 200 - 1 * var3 + rnorm(n.datos, sd = 20)
+
+
+datos <- rbind(data.table(target = target1, variable = var1, grupo = "1"),
+               data.table(target = target2, variable = var2, grupo = "2"),
+               data.table(target = target3, variable = var3, grupo = "3"))
+
+
+plot.otra.variable <- datos %>%
+  ggplot(aes(x = variable, y = target)) +
+  geom_smooth(method = "lm", se = FALSE, alpha = 0.5) +
+  geom_point() +
+  theme_bw() +
+  labs(y = "Target", x = "X1")
+
+ggsave("Imagenes/Teorica/Otra variable.png", plot.otra.variable, width = 4, height = 4)
+
+
+plot.otra.variable.color <- datos %>%
+  ggplot(aes(x = variable, y = target, col = grupo)) +
+  geom_smooth(method = "lm", se = FALSE, alpha = 0.5) +
+  geom_point() +
+  theme_bw() +
+  labs(y = "Target", x = "X1", col = "Grupo")
+
+ggsave("Imagenes/Teorica/Otra variable color.png", plot.otra.variable.color, width = 4, height = 4)
+
+
+modelo.otra.variable <- lm(target ~ variable, data = datos)
+
+modelo.otra.variable.aumentado <- augment(modelo.otra.variable)
+
+plot.modelo.otra.variable <- res.vs.pred(modelo.otra.variable.aumentado)
+
+ggsave("Imagenes/Teorica/Modelo otra variable.png", plot.modelo.otra.variable, width = 4, height = 4)
+
+
+
+modelo.otra.variable.color <- lm(target ~ variable * grupo, data = datos)
+
+modelo.otra.variable.color.aumentado <- augment(modelo.otra.variable.color)
+
+plot.modelo.otra.variable.color <- res.vs.pred(modelo.otra.variable.color.aumentado)
+
+ggsave("Imagenes/Teorica/Modelo otra variable color.png", plot.modelo.otra.variable.color, width = 4, height = 4)
+
+
+
+### Transformacion logaritmica:
+
+
+set.seed(3)
+
+n.datos <- 50
+
+var1 <- runif(n.datos, min = 5, max = 15)
+
+target <- 1 + var1 * 4 + rnorm(n.datos, sd = 1.5 * var1)
+
+datos <- data.table(target, var1)
+
+plot.transformacion <- datos %>%
+  ggplot(aes(x = var1, y = target)) +
+  geom_smooth(method = "lm", se = FALSE, alpha = 0.5) +
+  geom_point() +
+  theme_bw() +
+  labs(y = "Target", x = "X1")
+
+ggsave("Imagenes/Teorica/Plot transformacion.png", plot.transformacion, width = 4, height = 4)
+
+
+plot.transformado <- datos %>%
+  ggplot(aes(x = var1, y = log(target))) +
+  geom_smooth(method = "lm", se = FALSE, alpha = 0.5) +
+  geom_point() +
+  theme_bw() +
+  labs(y = "Log(Target)", x = "X1")
+
+ggsave("Imagenes/Teorica/Plot transformado.png", plot.transformado, width = 4, height = 4)
